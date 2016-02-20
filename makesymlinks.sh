a95bbace51a15d0598ffee45ed3fe236bb4c42b0
Tom Hipkin <tomhipkin@gmail.com>
2016-02-20 20:54:26 +0000

    fix it

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -34,7 +34,7 @@ install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my prezto repository
-    git clone --recursive https://github.com/tomhipkin/prezto.git "${ZDOTDIR:-$HOME/dotfiles}/.zprezto"
+    git clone --recursive https://github.com/tomhipkin/prezto.git "${ZDOTDIR:-$HOME/dotfiles}/prezto"
 
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
