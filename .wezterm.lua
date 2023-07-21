0827fce98e4f0e1106ce322e346a70a43c3a03f4
Tom Hipkin <tomhipkin@gmail.com>
2023-07-21 15:29:19 +0100

    Add word jumping shortcuts to wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,6 +12,14 @@ local config = {
   default_cursor_style = "BlinkingBlock",
   adjust_window_size_when_changing_font_size = false,
   initial_cols = 120,
+
+  -- Word jumping left and right
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
 }
 
 -- and finally, return the configuration to wezterm
